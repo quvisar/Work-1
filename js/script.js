99fdda1151c2c6b6335976028dcd64b49d115e80
quvisar <112892761+quvisar@users.noreply.github.com>
2022-11-29 23:09:32 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let btn = document.querySelector('.btn__video');
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.header__links'); 
 let items = document.querySelectorAll('.header__item'); 
+let body = document.querySelector('body'); 
 
 burger.addEventListener("click",action);
 menu.addEventListener("click",action);
@@ -13,7 +14,6 @@ if(window.innerWidth<=768)
 	btn.innerHTML="Видео-ролик";
 }
 function action(){
-	menu.classList.toggle('_active');  
+	menu.classList.toggle('_active');
+	body.classList.toggle('_active');  
 }
-
-console.log(items);
